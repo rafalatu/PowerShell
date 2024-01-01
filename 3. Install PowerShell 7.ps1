fb773ac705e9e8301dde7687adcb42bea70d744d
L00179720 <151082450+rafalatu@users.noreply.github.com>
2024-01-01 19:50:45 +0000

    Update 3. Install PowerShell 7.ps1
    I added a new parameter NoNewWindow

--- 3. Install PowerShell 7.ps1
+++ 3. Install PowerShell 7.ps1
@@ -1,7 +1,12 @@
-﻿$MYPARMS = @{
- UseMSI = $true
- Quiet = $true
- AddExplorerContextMenu = $true
- EnablePSRemoting = $true
- }
- C:\PowerShell\Install-PowerShell.ps1 @MYPARMS
+# Define parameters for the PowerShell installation script
+$MYPARMS = @{
+    UseMSI = $true
+    Quiet = $true
+    AddExplorerContextMenu = $true
+    EnablePSRemoting = $true
+    # Add a new parameter
+    NoNewWindow = $true
+}
+
+# Call the installation script with the defined parameters
+C:\PowerShell\Install-PowerShell.ps1 @MYPARMS
